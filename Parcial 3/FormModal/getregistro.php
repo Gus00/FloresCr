211def1ca94a0382fa7d944b647df67c18e2132c
Floresd <gusflores2701@hotmail.com>
2022-05-18 19:50:19 -0500

    Correjido Json

--- Parcial 3/FormModal/getregistro.php
+++ Parcial 3/FormModal/getregistro.php
@@ -1,8 +1,8 @@
 <?php
-$registro['nombre'] = 'Jose'
-$registro['apellidos'] = 'Madero'
-$registro['telefono'] = '8674641445'
-$registro['correo'] = 'correo@hotmail.com'
+$registro['nombre'] = 'Jose';
+$registro['apellidos'] = 'Madero';
+$registro['telefono'] = '8674641445';
+$registro['correo'] = 'correo@hotmail.com';
 
 $registroJson = json_encode($registro);
 echo $registroJson;
