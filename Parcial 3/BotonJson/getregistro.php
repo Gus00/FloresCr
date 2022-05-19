04c7cb3f2d17fb262fb816f077731c81aac5201b
Floresd <gusflores2701@hotmail.com>
2022-05-18 20:34:46 -0500

    Se creo la carpeta BotonJson

---
+++ Parcial 3/BotonJson/getregistro.php
@@ -0,0 +1,9 @@
+<?php
+$registro['nombre'] = 'Jose';
+$registro['apellidos'] = 'Madero';
+$registro['telefono'] = '8674641445';
+$registro['correo'] = 'correo@hotmail.com';
+
+$registroJson = json_encode($registro);
+echo $registroJson;
+?>
