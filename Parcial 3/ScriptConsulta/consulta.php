bae2cbd315301d10ab4814fd200b5320a57ef2d6
Floresd <gusflores2701@hotmail.com>
2022-05-23 19:47:28 -0500

    Se agrega ScriptConsulta

---
+++ Parcial 3/ScriptConsulta/consulta.php
@@ -0,0 +1,32 @@
+<?php
+
+$id=1;
+
+$hostname='localhost';
+$database='L19100180';
+$username='root';
+$password='2701';
+
+try {
+    $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
+
+} catch (PDOException $e) {
+   echo "Error de conexion a la base de datos;";
+   echo $e->getMessage();
+   exit();
+}
+
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    $consultaSql = "select * from comprador where id_Comprador=".$id;
+    $consulta = $con -> prepare($consultaSql);
+    $consulta -> execute();
+    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+    $consulta->closeCursor();
+
+} catch (PDOException $e) {
+echo "Error de consulta a la base de datos";
+echo $e->getMessage();
+}
+echo json_encode($resultado);
+?>
