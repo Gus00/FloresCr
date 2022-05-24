6aa67302471cca6263d19fd652f76fb136500ad9
Floresd <gusflores2701@hotmail.com>
2022-05-23 19:27:56 -0500

    SQL actualizado

--- Parcial 3/ScripSQL/L19100180.sql
+++ Parcial 3/ScripSQL/L19100180.sql
@@ -3,16 +3,18 @@ use L19100180;
 
 create table COMPRADOR 
 (
+id_Comprador smallint auto_increment,
 nombre varchar(30),
 apellidoMaterno varchar(30),
 apellidoPaterno varchar(30),
 numeroTelefono varchar(12),
 correoElectronico varchar(50),
 TipoBoleto varchar(10),
-asientoEspecial varchar(6)
+asientoEspecial varchar(6),
+constraint PK_ID_COMPRADOR primary key (id_Comprador)
 );
 
 
-insert into comprador values('David','Castillo','Tellez','867464532','correo@gmail.com','VIP','No');
-insert into comprador values('Ludwing','Oliva','Perea','867685345','ludwingdaniel.op@nlaredo.tecnm.mx','Normal','No');
+insert into comprador values(null,'David','Castillo','Tellez','867464532','correo@gmail.com','VIP','No');
+insert into comprador values(null,'Ludwing','Oliva','Perea','867685345','ludwingdaniel.op@nlaredo.tecnm.mx','Normal','No');
 
